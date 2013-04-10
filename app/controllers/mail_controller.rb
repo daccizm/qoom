ea2ea80e381d7538c543c3397e6f489dde52f73d
fukuishere <fukuishere@gmail.com>
2013-04-10 20:50:59 +0900

    add Question From Mail fix bug6

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -3,58 +3,58 @@ class MailController < ApplicationController
   skip_before_filter :authenticate_user!, :verify_authenticity_token
 
   def create
-    message = Mail.new(params[:message])
-    puts "*********************************"
-    puts message.inspect
-    puts "*********************************"
+    # message = Mail.new(params[:message])
+    # puts "*********************************"
+    # puts message.inspect
+    # puts "*********************************"
 
-    body = ""
-    encoding = ""
-    has_plain_text = false;
-    has_html_text = false;
+    # body = ""
+    # encoding = ""
+    # has_plain_text = false;
+    # has_html_text = false;
 
-    message.body.to_s.split("\n").each {|m|
+    # message.body.to_s.split("\n").each {|m|
  
-      if m =~ /charset=.*/
-        encoding = $&.gsub("charset=","")
-	    puts encoding
-	    puts "1 *********************************"
-      end
-      unless m.index("text/plain") == nil
-        has_plain_text = true
-	    puts "2 *********************************"
-      end
-      unless m.index("text/html") == nil
-        has_html_text = true
-	    puts "3 *********************************"
-      end
-      #debug
-      puts m
+    #   if m =~ /charset=.*/
+    #     encoding = $&.gsub("charset=","")
+	   #  puts encoding
+	   #  puts "1 *********************************"
+    #   end
+    #   unless m.index("text/plain") == nil
+    #     has_plain_text = true
+	   #  puts "2 *********************************"
+    #   end
+    #   unless m.index("text/html") == nil
+    #     has_html_text = true
+	   #  puts "3 *********************************"
+    #   end
+    #   #debug
+    #   puts m
  
-      if m.index("Content-Type") == nil
-        if m.index("Content-Transfer-Encoding") == nil
-          if m.index(/\-\-[0-9a-z]*/) == nil
-            body += m unless has_plain_text && has_html_text
-     	    puts "4 *********************************"
-          end
-        end
-      end
-    }
-    unless body.encoding.to_s == "UTF-8"
-      body = body.encode("UTF-8")
-      encoding = encoding.encode("UTF-8")
-      puts body
-      puts encoding
-      puts "5 *********************************"
-    end
+    #   if m.index("Content-Type") == nil
+    #     if m.index("Content-Transfer-Encoding") == nil
+    #       if m.index(/\-\-[0-9a-z]*/) == nil
+    #         body += m unless has_plain_text && has_html_text
+    #  	    puts "4 *********************************"
+    #       end
+    #     end
+    #   end
+    # }
+    # unless body.encoding.to_s == "UTF-8"
+    #   body = body.encode("UTF-8")
+    #   encoding = encoding.encode("UTF-8")
+    #   puts body
+    #   puts encoding
+    #   puts "5 *********************************"
+    # end
  
     begin
       #debug
       puts "start"
-      puts message.subject
-      puts body
-	  room = Room.find_by_account( message.subject )
-      question = room.questions.new(:content => body)
+      puts params[:subject]
+      puts params[:plain]
+	  room = Room.find_by_account( params[:subject] )
+      question = room.questions.new(:content => params[:plain] )
       if question.save
         puts "OK"
         render :text => "ok"

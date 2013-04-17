dbcfeee96240b811691c33cc5048234b5c161a20
fukuda <fukuda.hiroyuki@tpj.co.jp>
2013-04-17 12:31:56 +0900

    edit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.delete_all
-UsersRoom.delete_all
-Room.delete_all
-RoomsQuestion.delete_all
-Question.delete_all
+# User.delete_all
+# UsersRoom.delete_all
+# Room.delete_all
+# RoomsQuestion.delete_all
+# Question.delete_all
+# Cooperation.delete_all
 
-Cooperation.delete_all
+tpj = User.where(:account=>'TPJ')
+unless tpj
+  user = User.create( account: 'TPJ', password: 'tpj!', password_confirmation: 'tpj!' )
+  room = user.rooms.create( name: 'demo' )
+  room.cooperations.create( account: 'demo' )
+end
 
-user = User.create( account: 'TPJ', password: 'tpj!', password_confirmation: 'tpj!' )
-room = user.rooms.create( name: 'demo' )
-room.cooperations.create( account: 'demo' )
+tpjmed = User.where(:account=>'TPJMED')
+unless tpj
+  user = User.create( account: 'TPJMED', password: 'tpjmed!', password_confirmation: 'tpjmed!' )
+  room = user.rooms.create( name: '2013/04/22 医療部会' )
+end

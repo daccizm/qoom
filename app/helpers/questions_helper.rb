62188888e98e8502ec0aa2e632421b5c985c8ca3
fukuda <fukuda.hiroyuki@tpj.co.jp>
2013-06-21 12:25:18 +0900

    fixed #27

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,13 @@
 module QuestionsHelper
+  def print_content(text)
+    raw(url2link(html_escape(text).gsub(/\r\n|\r|\n/, "<br />")))
+  end
+
+  def url2link(text, options={})
+    target = options[:target] || '_blank'
+    URI.extract(text).uniq.each{|url|
+      text.gsub!(url, "<a href='#{url}' target='#{target}'>#{truncate(url, :length => 40)}</a>")
+    }
+    return text
+  end
 end
